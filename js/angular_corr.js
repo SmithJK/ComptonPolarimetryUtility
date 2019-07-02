42559b2e212103514d9adf39064a6696afd2a5e6
SmithJK <smith.jenna.kathleen@gmail.com>
2019-07-02 10:47:41 -0700

    Update angular_corr.js

--- js/angular_corr.js
+++ js/angular_corr.js
@@ -312,7 +312,6 @@ function calculate_P90(j1, p1, j2, p2, j3, p3, l1a, l1b, l2a, l2b, delta1, delta
    }
    var bottomterm = 1;
    for (i=2;i<9;i=i+2) {
-      //firstterm = legendre(i,0);
       firstterm = A(i,j1,j2,l1a,l1b,delta1)*B(i,j2,j3,l2a,l2b,delta2)*legendre(i,0);
       bottomterm = bottomterm + firstterm;
    }
@@ -320,7 +319,7 @@ function calculate_P90(j1, p1, j2, p2, j3, p3, l1a, l1b, l2a, l2b, delta1, delta
    // this is just a fancy way of making sure that an electric transition for level 2-->3 gives us the + factor and a magnetic transition gives us the - factor.
    var factor = (-2*(l2a%2)+1)*p2*p3;
 
-   return bottomterm;
+   return topterm;
 //   return factor*topterm/bottomterm;
 }
 
